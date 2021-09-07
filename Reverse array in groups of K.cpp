52657b63cdc5ac4d9adf40087951335a22dce3e0
Satvik srivastava <51355603+divinityiitj@users.noreply.github.com>
2021-09-07 20:36:09 +0530

    Create Reverse array in groups of K.cpp

---
+++ Reverse array in groups of K.cpp
@@ -0,0 +1,21 @@
+include <iostream>
+using namespace std;
+ 
+// Function to reverse every sub-array formed by
+// consecutive k elements
+void reverse(int arr[], int n, int k)
+{
+    for (int i = 0; i < n; i += k)
+    {
+        int left = i;
+ 
+        // to handle case when k is not multiple of n
+        int right = min(i + k - 1, n - 1);
+ 
+        // reverse the sub-array [left, right]
+        while (left < right)
+            swap(arr[left++], arr[right--]);
+ 
+    }
+}
+ 
